97339e1b04903f7449cba0bc8487f10cfb13ff87
AbdassalamAhmad <abdassalam.ahmad98@gmail.com>
2022-09-06 16:15:58 +0200

    change mongo from localhost to docker

--- Docker/demo_app/server.js
+++ Docker/demo_app/server.js
@@ -35,7 +35,7 @@ let databaseName = "user-account";
 app.post('/update-profile', function (req, res) {
   let userObj = req.body;
 
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+  MongoClient.connect(mongoUrlDocker, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
     let db = client.db(databaseName);
@@ -57,7 +57,7 @@ app.post('/update-profile', function (req, res) {
 app.get('/get-profile', function (req, res) {
   let response = {};
   // Connect to the db
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+  MongoClient.connect(mongoUrlDocker, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
     let db = client.db(databaseName);
