ac52781373a6759ed699765920afca6289c6c03d
AbdassalamAhmad <abdassalam.ahmad98@gmail.com>
2022-09-06 16:24:28 +0200

    change variable names

--- Docker/demo_app/py/mongodb.py
+++ Docker/demo_app/py/mongodb.py
@@ -5,20 +5,20 @@ options = Options()
 options.add_argument("--window-size=1920,1080")
 driver = webdriver.Chrome(options=options)
 
-# open mongo-express.
+# open mongo-express and press on user-account database.
 driver.get("http://localhost:8080/")
-folder= driver.find_element("xpath", "//tbody//tr//td//h3//a[normalize-space()='user-account']")
-folder.click()
-
-# press on user-account database.
-driver.get("http://localhost:8080/db/user-account/")
-folder1= driver.find_element("xpath", "/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/div[2]/table[1]/tbody[1]/tr[2]/td[5]/h3[1]/a[1]")
-folder1.click()
+user_account= driver.find_element("xpath", "//tbody//tr//td//h3//a[normalize-space()='user-account']")
+user_account.click()
 
 # press on users collection.
+driver.get("http://localhost:8080/db/user-account/")
+users_collection= driver.find_element("xpath", "/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/div[2]/table[1]/tbody[1]/tr[2]/td[5]/h3[1]/a[1]")
+users_collection.click()
+
+# press to see updates.
 driver.get("http://localhost:8080/db/user-account/users")
-folder2= driver.find_element("xpath", "/html[1]/body[1]/div[1]/div[2]/div[1]/div[4]/table[1]/tbody[1]/tr[1]/td[3]/div[1]")
-folder2.click()
+updates= driver.find_element("xpath", "/html[1]/body[1]/div[1]/div[2]/div[1]/div[4]/table[1]/tbody[1]/tr[1]/td[3]/div[1]")
+updates.click()
 
 # open a new tab.
 driver.execute_script("window.open('about:blank', 'secondtab');")
@@ -26,13 +26,13 @@ driver.switch_to.window("secondtab")
   
 # open my web-app and press edit Button.
 driver.get("http://localhost:3000/")
-folder3 = driver.find_element("xpath", "/html[1]/body[1]/div[1]/button[1]")
-folder3.click()
+Edit_Button = driver.find_element("xpath", "/html[1]/body[1]/div[1]/button[1]")
+Edit_Button.click()
 
 # add Gaming to the interests.
-folder4 = driver.find_element("xpath", "/html[1]/body[1]/div[2]/input[3]")
-folder4.send_keys(", Gaming.")
+Gaming = driver.find_element("xpath", "/html[1]/body[1]/div[2]/input[3]")
+Gaming.send_keys(", Gaming.")
 
 # click on Update Button.
-folder5= driver.find_element("xpath", "/html[1]/body[1]/div[2]/button[1]")
-folder5.click()
+Update_Button= driver.find_element("xpath", "/html[1]/body[1]/div[2]/button[1]")
+Update_Button.click()
