89daa788e402cff5d255a74b9d4dae0caf074915
AbdassalamAhmad <abdassalam.ahmad98@gmail.com>
2023-01-03 13:57:32 +0200

    format

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -4,17 +4,17 @@ output "load_balancer_dns" {
 }
 
 output "bastion_public_ip" {
-  value     = aws_instance.bastion_instance_1.public_ip
+  value = aws_instance.bastion_instance_1.public_ip
   #sensitive = true
 }
 
 output "application_private_ip_1" {
-  value     = aws_instance.application_instance_1.private_ip
+  value = aws_instance.application_instance_1.private_ip
   #sensitive = true
 }
 
 output "application_private_ip_2" {
-  value     = aws_instance.application_instance_2.private_ip
+  value = aws_instance.application_instance_2.private_ip
   #sensitive = true
 }
 
