a2a8d79f26300a5aaec06e0ba8c0aba307777033
AbdassalamAhmad <abdassalam.ahmad98@gmail.com>
2023-01-08 22:54:15 +0200

    add ssh keygen method

--- Terraform/key_pair.tf
+++ Terraform/key_pair.tf
@@ -6,24 +6,7 @@ resource "aws_key_pair" "ssh_key_variable" {
   public_key = var.public_key
 }
 
-
-
-# resource "tls_private_key" "ec2_key" {
-#   algorithm = "RSA"
-#   rsa_bits  = 4096
-# }
-
-# resource "aws_key_pair" "ssh_key" {
-#   key_name   = "ssh_ec2_key"
-#   public_key = tls_private_key.ec2_key.public_key_openssh
-
-# }
-
-# resource "local_file" "ssh_key" {
-#   filename = "E:/keys/pk.pem"
-#   content  = tls_private_key.ec2_key.private_key_pem
-#   provisioner "local-exec" {
-#     interpreter = ["bash", "-c"]
-#     command     = "chmod 400 E:/keys/pk.pem"
-#   }
-# }
+# ssh-keygen # name = ssh_key_aws
+# chmod 400 ssh_key_aws
+# cat ssh_key_aws >> GitHub Secrets EC2_PRIVATE_SSH_KEY
+# cat ssh_key_aws.pub >> GitHub Secrets EC2_PUBLIC_SSH_KEY
