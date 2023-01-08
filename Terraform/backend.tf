32d9b06f85bf470a0fcf05cac751497ffea4c5cf
AbdassalamAhmad <abdassalam.ahmad98@gmail.com>
2023-01-08 22:53:55 +0200

    add comment about encrypt

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -4,5 +4,6 @@ terraform {
     key            = "terraform.tfstate"   # path and name of state file.
     region         = "eu-south-1"
     dynamodb_table = "state_table" # name of dynamodb table for State Lock, must have partition key = "LockID"
+    # encrypt = true # by default
   }
 }
